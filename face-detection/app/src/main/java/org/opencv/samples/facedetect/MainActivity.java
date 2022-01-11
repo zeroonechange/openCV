ff902ba7106e7eeaf41999591f808cd3b457fc19
zeroone <zeroonechange@gmail.com>
2022-01-11 21:52:54 +0800

    init

---
+++ face-detection/app/src/main/java/org/opencv/samples/facedetect/MainActivity.java
@@ -0,0 +1,36 @@
+package org.opencv.samples.facedetect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.opencv.samples.facedetect.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'facedetect' library on application startup.
+    static {
+        System.loadLibrary("facedetect");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'facedetect' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
